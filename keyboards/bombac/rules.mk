8319fae24fcd51c006137db8bbe069fe22084796
manimtired1312 <nthncameron@gmail.com>
2022-04-19 22:59:56 -0500

    V1 firmware

---
+++ keyboards/bombac/rules.mk
@@ -0,0 +1,16 @@
+# MCU name
+MCU = atmega32u4
+
+# Bootloader selection
+BOOTLOADER = atmel-dfu
+
+# Build Options
+BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
+MOUSEKEY_ENABLE = yes       # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+CONSOLE_ENABLE = no         # Console for debug
+COMMAND_ENABLE = no         # Commands for debug and configuration
+NKRO_ENABLE = yes           # Enable N-Key Rollover
+KEY_OVERRIDE_ENABLE = yes    # enable key overrides-
+TAP_DANCE_ENABLE = no        # tap-dancccc
+SPLIT_KEYBOARD = yes
